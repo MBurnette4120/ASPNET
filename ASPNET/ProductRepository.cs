03d7a94772397751724c84517e96929ca646ead0
MBurnette4120 <mikeburnette@rocketmail.com>
2020-07-28 15:03:55 -0400

    IDBConnectionAdded

---
+++ ASPNET/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using ASPNET.Models;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace ASPNET
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }    
+        
+        
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
