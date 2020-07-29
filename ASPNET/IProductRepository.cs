220ad8242499c131eedf0b839f4e3b09b6faca99
MBurnette4120 <mikeburnette@rocketmail.com>
2020-07-29 12:39:10 -0400

    stage4step7

--- ASPNET/IProductRepository.cs
+++ ASPNET/IProductRepository.cs
@@ -11,6 +11,12 @@ namespace ASPNET
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
         public void UpdateProduct(Product product);
-        
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+
+
+
+
     }
 }
